adafd123c0db59e6bbad92dadc5970a4de9e358c
anli5005 <li.anthony917@gmail.com>
2014-09-03 15:28:54 -0400

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,4 +1,11 @@
 -- Define classes
-local Matrix = D.NewClass(D.Object)
-function Matrix:new()
+Matrix = D.NewClass(D.Object)
+function Matrix:new(rows, columns)
+  local obj = D.Object:new()
+  obj.rows = rows
+  obj.columns = columns
+  obj.values = []
+  for i = 1, rows * columns
+    obj.values[i] = 0
+  end
 end
